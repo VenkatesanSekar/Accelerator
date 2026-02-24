d1b4c2a36f65601e3376c06d9676cf4bf24388d7
VenkatesanSekar <venkatsekar1312@gmail.com>
2026-02-24 23:30:10 +0530

    adding new src
    adding new src

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { ServiceDetail } from './pages/service-detail/service-detail';
 import { Home } from './home/home';
 import { About } from './about/about';
 import { Differentiators} from './differentiators/differentiators';
+import { ContactUs } from './contact-us/contact-us';
 
 export const routes: Routes = [
     { path: 'servicedetails', component: ServiceDetail },
     { path: 'home', component: Home },
     { path: 'about', component: About },
     { path: 'differentiators', component: Differentiators },
+     { path: 'contact-us', component: ContactUs },
 ];
--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -3,15 +3,17 @@ import { RouterLink } from '@angular/router';
 import { ServiceDetail } from '../pages/service-detail/service-detail';
 import { Home } from '../home/home';
 import { About } from '../about/about';
+import { ContactUs } from '../contact-us/contact-us';
 
 @Component({
   selector: 'app-header',
-   imports: [RouterLink, ServiceDetail,Home,About],
+   imports: [RouterLink, ServiceDetail,Home,About,ContactUs],
   standalone: true,
   templateUrl: './header.html',
 })
 export class Header {// Signal to track if the 'Pathways' dropdown is open
   isPathwaysOpen = signal(false);
+  isMenuOpen = false;
 
   togglePathways() {
     this.isPathwaysOpen.update(val => !val);
